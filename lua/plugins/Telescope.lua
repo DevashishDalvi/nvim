fd9d8f856c27471937e7b52a003fd717d0bebbe1
Devashish Dalvi <92207754+DevashishDalvi@users.noreply.github.com>
2025-11-09 12:17:35 +0530

    Update Telescope key mappings and themes
    Refactor key mappings for Telescope with themes and options.

--- lua/plugins/Telescope.lua
+++ lua/plugins/Telescope.lua
@@ -84,6 +84,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
+    local themes = require 'telescope.themes'
     local actions = require 'telescope.actions'
     local action_state = require 'telescope.actions.state'
 
@@ -144,21 +145,44 @@ return { -- Fuzzy Finder (files, lsp, etc)
       }
     end, { desc = '[S]earch [H]elp (float)' })
     
-    --vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    -- vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
     vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    
+    -- vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    vim.keymap.set('n', '<leader>sf', function()
+      local opts = themes.get_ivy {
+        layout_config = {
+          -- border = 'rounded',
+          height = 13,
+        },
+        initial_mode = 'normal',
+        winblend = 10,
+      }
+      builtin.find_files(opts)
+    end, { desc = '[S]earch [F]iles' })
+
     vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
     vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader><leader>', function()
+      local opts = themes.get_ivy {
+        layout_config = {
+          height = 15,
+        },
+        initial_mode = 'normal',
+        -- previewer = false,
+      }
+      builtin.buffers(opts)
+    end, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+      builtin.current_buffer_fuzzy_find(themes.get_dropdown {
         winblend = 10,
         previewer = false,
       })
